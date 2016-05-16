8ff3fe196422e3d9cb7405cb4915b034eb1ad613
Conrad Stansbury <chstansbury@gmail.com>
2016-05-15 20:18:16 -0700

    Add routes for Trello to available ones

--- src/clj/new_website/server.clj
+++ src/clj/new_website/server.clj
@@ -1,15 +1,19 @@
 (ns new-website.server
   (:require [clojure.java.io :as io]
-            [compojure.core :refer [ANY GET PUT POST DELETE defroutes]]
+            [compojure.core :refer [ANY GET PUT POST DELETE defroutes routes]]
             [compojure.route :refer [resources]]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
             [ring.middleware.gzip :refer [wrap-gzip]]
             [ring.middleware.logger :refer [wrap-with-logger]]
+            [ring.middleware.params :refer [wrap-params]]
+            [ring.middleware.format :refer [wrap-restful-format]]
+            [ring.middleware.anti-forgery :refer [*anti-forgery-token*]]
             [environ.core :refer [env]]
-            [ring.adapter.jetty :refer [run-jetty]])
+            [ring.adapter.jetty :refer [run-jetty]]
+            [new-website.trello :refer [trello-routes]])
   (:gen-class))
 
-(defroutes routes
+(defroutes core-routes
   (GET "/anti_forgery.js" [req]
     {:status 200
      :headers {"Content-Type" "application/javascript"}
@@ -21,10 +25,14 @@
   (resources "/"))
 
 (def http-handler
-  (-> routes
-      (wrap-defaults site-defaults)
-      wrap-with-logger
-      wrap-gzip))
+  (routes
+    (-> core-routes
+        (wrap-defaults site-defaults)
+        wrap-with-logger
+        wrap-gzip)
+    (-> trello-routes
+        (wrap-restful-format :formats [:json :transit-json :edn])
+        wrap-with-logger)))
 
 (defn -main [& [port]]
   (let [port (Integer. (or port (env :port) 10555))]
