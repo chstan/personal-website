62c2265f4e6e5c6e6621d93b162bda38bf8037b0
Conrad Stansbury <chstansbury@gmail.com>
2025-12-19 16:00:58 -0800

    cleanup: remove CRA artifacts and update entry point

--- src/index.tsx
+++ src/index.tsx
@@ -5,9 +5,6 @@ import ReactGA from 'react-ga';
 import './css/index.css';
 import App from './App';
 
-import * as serviceWorker from './serviceWorker';
-
 ReactGA.initialize('UA-55955707-3');
 
 ReactDOM.render(<App />, document.getElementById('app'));
-serviceWorker.unregister();
